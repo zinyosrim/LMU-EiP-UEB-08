7d0e00030536eadf4b40adccfef8479dd7885ea5
zinyosrim <zinyosrim@gmail.com>
2016-12-12 13:41:18 +0100

    add caesar chiffre

--- src/Passwort.java
+++ src/Passwort.java
@@ -16,11 +16,33 @@ public class Passwort {
 				System.out.println(code(eingabe, i));
 		else
 			System.out.println(code(eingabe, index));
+		
+
+		
+		
 	}	
 
 	public static String code(String eingabe, int index){
 		String ausgabe = "";
 		// TODO
+		
+		// durchlaufe den Eingabe-String char by char
+		for (int i=0;i<eingabe.length();i++){
+			
+			if (!((int)eingabe.charAt(i) < 123 && (int)eingabe.charAt(i) > 96 )){
+				// wenn Zeichen ausser a..z enthalten sind, transformiere diese nicht
+				ausgabe = ausgabe + eingabe.charAt(i);
+			}
+			else if ((char)(int)eingabe.charAt(i)+index < 123) {
+					// wenn bei der Verschiebung kein Überlauf stattfindet, nehme das i+index te Element
+					ausgabe = ausgabe + (char)((int)(eingabe.charAt(i) + index));
+				}
+				else{
+					// Überlauf!Mache nach z bei a weiter 
+					ausgabe = ausgabe + (char)((int)(eingabe.charAt(i)+index-26));
+				}
+
+		}
 		return ausgabe;
 	}
 }
---
+++ src/passwort2.java
@@ -0,0 +1,17 @@
+
+public class passwort2 {
+
+	public static String code(String eingabe, int index){
+		String ausgabe = "";
+		ausgabe = eingabe+"deniz";
+		// TODO
+		return ausgabe;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(code("eingabe", 0));
+
+	}
+
+}
